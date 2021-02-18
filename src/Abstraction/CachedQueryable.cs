7c67bfb75d8deaae316dd77e2ac6a7a2a0c53603
XiaoYang <670171961@qq.com>
2021-02-18 19:04:28 +0800

    Move cacheable features namespace to MS.EFC.Cacheable.

--- src/Abstraction/CachedQueryable.cs
+++ src/Abstraction/CachedQueryable.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Microsoft.EntityFrameworkCore
+namespace Microsoft.EntityFrameworkCore.Cacheable
 {
     /// <summary>
     /// The cached queryable extension methods.
--- test/Cacheable/CacheableFunctional.cs
+++ test/Cacheable/CacheableFunctional.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Cacheable;
 using System;
 using System.Threading.Tasks;
 using Xunit;
