717a17339c0b92f43edb58102e3c5c048b72d491
XiaoYang <670171961@qq.com>
2021-02-18 19:12:33 +0800

    Add testcases for alternative key.

--- test/Common/UpsertRelated.cs
+++ test/Common/UpsertRelated.cs
@@ -23,11 +23,28 @@ namespace Testcase_Upsert
         public bool Public { get; set; }
     }
 
+    public class TwoRelation
+    {
+        public int AaaId { get; set; }
+        public int BbbId { get; set; }
+    }
+
+    public class ThreeRelation
+    {
+        public int AaaId { get; set; }
+        public int BbbId { get; set; }
+        public int PkeyId { get; set; }
+    }
+
     public class UpsertContext : DbContext
     {
         public DbSet<RankCache> RankCache { get; set; }
 
         public DbSet<RankSource> RankSource { get; set; }
+
+        public DbSet<TwoRelation> TwoRelations { get; set; }
+
+        public DbSet<ThreeRelation> ThreeRelations { get; set; }
 
         public string DefaultSchema { get; }
 
@@ -51,6 +68,26 @@ namespace Testcase_Upsert
                 entity.ToTable(nameof(RankSource) + "_" + DefaultSchema);
 
                 entity.HasKey(e => new { e.ContestId, e.TeamId });
+            });
+
+            modelBuilder.Entity<TwoRelation>(entity =>
+            {
+                entity.ToTable(nameof(TwoRelation) + "_" + DefaultSchema);
+
+                entity.HasKey(e => new { e.BbbId, e.AaaId });
+
+                entity.HasIndex(e => e.AaaId);
+            });
+
+            modelBuilder.Entity<ThreeRelation>(entity =>
+            {
+                entity.ToTable(nameof(ThreeRelation) + "_" + DefaultSchema);
+
+                entity.HasKey(e => e.PkeyId);
+
+                entity.HasAlternateKey(e => new { e.BbbId, e.AaaId });
+
+                entity.HasIndex(e => e.AaaId);
             });
         }
     }
@@ -186,6 +223,36 @@ namespace Testcase_Upsert
                 rc2 => new RankCache { PointsPublic = 1, PointsRestricted = 1, TotalTimePublic = rc2.Time, TotalTimeRestricted = rc2.Time, ContestId = rc2.ContestId, TeamId = rc2.TeamId, });
 
             Assert.Equal(3, context.RankCache.Count());
+        }
+
+        [Fact(Skip = "Future"), TestPriority(5)]
+        public void Upsert_Bug202102102024()
+        {
+            using var context = contextFactory();
+
+            Assert.Equal(0, context.TwoRelations.Count());
+
+            int aaa = 1, bbb = 1;
+            var e = context.TwoRelations.Upsert(
+                new { aaa, bbb },
+                s => new TwoRelation { BbbId = bbb, AaaId = aaa });
+
+            Assert.Equal(1, context.TwoRelations.Count());
+        }
+
+        [Fact, TestPriority(6)]
+        public void Upsert_AlternativeKey()
+        {
+            using var context = contextFactory();
+
+            Assert.Equal(0, context.ThreeRelations.Count());
+
+            int aaa = 1, bbb = 1;
+            var e = context.ThreeRelations.Upsert(
+                new { aaa, bbb },
+                s => new ThreeRelation { BbbId = s.bbb, AaaId = s.aaa });
+
+            Assert.Equal(1, context.ThreeRelations.Count());
         }
     }
 }
