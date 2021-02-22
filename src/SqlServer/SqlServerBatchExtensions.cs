2f272cfbff5d018cf0a8ccca25ca714123da2455
XiaoYang <670171961@qq.com>
2021-02-22 18:06:19 +0800

    Explore more.

--- src/SqlServer/SqlServerBatchExtensions.cs
+++ src/SqlServer/SqlServerBatchExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Microsoft.EntityFrameworkCore
 {
@@ -11,6 +12,7 @@ namespace Microsoft.EntityFrameworkCore
         private static void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IMethodCallTranslatorPlugin, MathTranslation>();
+            services.Replace(ServiceDescriptor.Singleton<IRelationalParameterBasedSqlProcessorFactory, XysParameterBasedSqlProcessorFactory>());
         }
 
         public static SqlServerDbContextOptionsBuilder UseBulk(this SqlServerDbContextOptionsBuilder builder)
