bbbb76bf9bf86476cd27add51c15ee61fc8720a4
XiaoYang <670171961@qq.com>
2021-02-25 23:57:03 +0800

    Compile for update in InMemory.

---
+++ src/InMemory/Bulk/BulkOperationBase.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.EntityFrameworkCore.Bulk
+{
+    public abstract class BulkOperationBase<TEntity> : IBulkQueryExecutor
+    {
+        protected readonly object _queryEnumerable;
+        protected readonly DbContext _dbContext;
+        protected readonly QueryContext _queryContext;
+        private CancellationToken _cancellationToken;
+
+        protected BulkOperationBase(QueryContext queryContext, object queryExecutor)
+        {
+            _queryEnumerable = queryExecutor;
+            _queryContext = queryContext;
+            _dbContext = queryContext.Context;
+            _cancellationToken = queryContext.CancellationToken;
+        }
+
+        protected abstract void Process(TEntity entity);
+
+        public virtual int Execute()
+        {
+            foreach (var entry in (IEnumerable<TEntity>)_queryEnumerable)
+            {
+                Process(entry);
+            }
+
+            return _dbContext.SaveChanges();
+        }
+
+        public virtual async Task<int> ExecuteAsync()
+        {
+            await foreach (var entry in (IAsyncEnumerable<TEntity>)_queryEnumerable)
+            {
+                Process(entry);
+            }
+
+            return await _dbContext.SaveChangesAsync(_cancellationToken);
+        }
+
+        public IBulkQueryExecutor WithCancellationToken(CancellationToken cancellationToken)
+        {
+            if (_cancellationToken == default)
+            {
+                _cancellationToken = cancellationToken;
+            }
+            else if (cancellationToken != default)
+            {
+                _cancellationToken = CancellationTokenSource
+                    .CreateLinkedTokenSource(cancellationToken, _cancellationToken)
+                    .Token;
+            }
+
+            return this;
+        }
+    }
+}
---
+++ src/InMemory/Query/ParameterAccessVisitor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.InMemory.Query.Internal;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class InMemoryParameterAccessVisitor : ExpressionVisitor
+    {
+        private const string CompiledQueryParameterPrefix = "__";
+
+        private static readonly MethodInfo translating_getParameterValue
+            = typeof(InMemoryExpressionTranslatingExpressionVisitor)
+                .GetTypeInfo().GetDeclaredMethod("GetParameterValue");
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (node.Name?.StartsWith(CompiledQueryParameterPrefix, StringComparison.Ordinal) == true)
+            {
+                return Expression.Call(
+                    translating_getParameterValue.MakeGenericMethod(node.Type),
+                    QueryCompilationContext.QueryContextParameter,
+                    Expression.Constant(node.Name));
+            }
+
+            return base.VisitParameter(node);
+        }
+
+        public static Expression Process(Expression origin)
+        {
+            return new InMemoryParameterAccessVisitor().Visit(origin);
+        }
+    }
+}
