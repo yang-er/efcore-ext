e3fc1f1f2fb0ad3b6b052289713fd621806ed106
XiaoYang <670171961@qq.com>
2021-02-27 15:56:40 +0800

    Fix conditional skipping.

--- test/TestUtilities/DatabaseProvider.cs
+++ test/TestUtilities/DatabaseProvider.cs
@@ -6,9 +6,26 @@ namespace Microsoft.EntityFrameworkCore.TestUtilities.Xunit
     public enum DatabaseProvider
     {
         None = 0,
-        InMemory = 1 << 0,
-        SqlServer = 1 << 1,
-        PostgreSQL = 1 << 2,
+
+        InMemory_31 = 1 << 0,
+        InMemory_50 = 1 << 1,
+
+        SqlServer_31 = 1 << 2,
+        SqlServer_50 = 1 << 3,
+
+        PostgreSQL_31 = 1 << 4,
+        PostgreSQL_50 = 1 << 5,
+
+        InMemory = InMemory_31 | InMemory_50,
+        SqlServer = SqlServer_31 | SqlServer_50,
+        PostgreSQL = PostgreSQL_31 | PostgreSQL_50,
+
+        Relational31 = SqlServer_31 | PostgreSQL_31,
+        Relational50 = SqlServer_50 | PostgreSQL_50,
         Relational = SqlServer | PostgreSQL,
+
+        Version_31 = InMemory_31 | Relational31,
+        Version_50 = InMemory_50 | Relational50,
+        All = InMemory | Relational
     }
 }
--- test/TestUtilities/DatabaseProviderSkipConditionAttribute.cs
+++ test/TestUtilities/DatabaseProviderSkipConditionAttribute.cs
@@ -10,8 +10,6 @@ namespace Microsoft.EntityFrameworkCore.TestUtilities.Xunit
     {
         public DatabaseProvider ExcludedProviders { get; }
 
-        public EFCoreVersion SkipVersion { get; set; }
-
         public DatabaseProviderSkipConditionAttribute(DatabaseProvider excludedProviders)
         {
             ExcludedProviders = excludedProviders;
@@ -21,19 +19,16 @@ namespace Microsoft.EntityFrameworkCore.TestUtilities.Xunit
         {
             var dir = method.Method.Type.Assembly.Name;
 
-            return dir.Contains("InMemory") ? DatabaseProvider.InMemory
+            var dp = dir.Contains("InMemory") ? DatabaseProvider.InMemory
                 : dir.Contains("PostgreSql") ? DatabaseProvider.PostgreSQL
                 : dir.Contains("SqlServer") ? DatabaseProvider.SqlServer
                 : DatabaseProvider.None;
-        }
 
-        private static EFCoreVersion GetCurrentVersion(ITestMethod method)
-        {
-            var dir = method.Method.Type.Assembly.Name;
+            var ver = dir.Contains("3.1") ? DatabaseProvider.Version_31
+                : dir.Contains("5.0") ? DatabaseProvider.Version_50
+                : DatabaseProvider.None;
 
-            return dir.Contains("3.1") ? EFCoreVersion.Version_3_1
-                : dir.Contains("5.0") ? EFCoreVersion.Version_5_0
-                : EFCoreVersion.None;
+            return dp & ver;
         }
 
         public string SkipReason => "Test cannot run on this provider.";
@@ -41,8 +36,7 @@ namespace Microsoft.EntityFrameworkCore.TestUtilities.Xunit
         public ValueTask<bool> IsMetAsync(XunitTestCase testcase)
         {
             return new ValueTask<bool>(
-                (GetCurrentProvider(testcase.TestMethod) & ExcludedProviders) == DatabaseProvider.None
-                && (GetCurrentVersion(testcase.TestMethod) & SkipVersion) == EFCoreVersion.None);
+                (GetCurrentProvider(testcase.TestMethod) & ExcludedProviders) == DatabaseProvider.None);
         }
     }
 }
