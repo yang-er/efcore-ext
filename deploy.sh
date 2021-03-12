11785b569f144bccaecb3a9650be3108cc4266ad
XiaoYang <670171961@qq.com>
2021-03-12 14:58:44 +0800

    Update package name.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUGET_KEY=$1
 cd packages
-dotnet nuget push --skip-duplicate -s https://nuget.xylab.fun/v3/index.json -k $NUGET_KEY "*.nupkg"
+dotnet nuget push -k $NUGET_KEY "*.nupkg"
